8d6516fd5b841ce0afe2bb4306c1acdc3fe50553
Taha <tahixos@gmail.com>
2020-06-29 17:02:30 +0100

    used

---
+++ Brief 11/README.md
@@ -0,0 +1,26 @@
+<h2>plugin_wpdb</h2>
+
+
+<h3>1 - Description </h3>
+
+Create a WordPress plugin with a configuration page with a settings page for the administration part of the created plugin (backoffice). 
+The plugin has 2 submenus: one for general information of the plugin and one for settings. The configuration page has an input text field, 
+textarea for the description, a list of options and a save button in the DB.
+
+<h3> 2 - How To Run?? </h3>
+
+To run this project you must follow these steps :
+  
+  <h4>1st Step: Extract files.</h4>  
+  <h4>2nd Step: Copy the plugin files.</h4> 
+  
+  <h4>3rd Step: create a folder in xampp\htdocs\wp-content\plugins in your wordpress instalation folder the folder must be named "wp_plugin".</h4> 
+  
+  <h4>4rd Step: copy all this fils and Paste in wp-content\plugins\wp_plugin in your wordpress instalation.</h4> 
+  
+  <h4>5td step: Activite the plugin from the dashboard of wordpress</h4> 
+ 
+  
+  
+
+
---
+++ Brief 11/wp_plugin/wp-content/plugins/wp_plugin/index.php
@@ -0,0 +1,163 @@
+<?php
+/*
+Plugin Name: My Plugin
+Plugin URI: 
+Description: First Plugin 
+Version: 0.0.1
+Author: TAHA
+Author URI: https://wp_plugin.com/wordpress-my-plugins/
+License: GPLv2 or later
+*/
+
+add_action("admin_menu","addMenu");
+
+
+function addMenu() {
+    add_menu_page( 'MonPlugin', 'MonPlugin', 'read', 'MonPlugin_Dashboard', 'MonPlugin_index' );
+    add_submenu_page( 'MonPlugin_Dashboard', 'MonPlugin', 'Settings', 'read', 'MonPlugin_Dashboard', 'MonPlugin_index');
+    add_submenu_page( 'MonPlugin_Dashboard', 'MonPlugin', 'info', 'read', 'MonPlugin_NouvellePage', 'MonPlugin_NouvellePage');
+}
+
+
+
+function MonPlugin_index(){
+  require_once( dirname( dirname( dirname( dirname( __FILE__ )))) . '/wp-load.php' );
+  global $wpdb;
+  if(isset($_POST['submit'])){
+      $option=$_POST['option'];
+      $name = $_POST['inputtxt'];
+      $email = $_POST['inputarea'];
+$table_name = $wpdb->prefix . "plugin_table";
+
+$wpdb->insert( $table_name, array(
+                      'inputtxt' => $name,
+                      'inputarea' => $email,
+                      'option'=>$option
+                      )); 
+  echo '
+    <div id="message" class="updated below-h2">
+      <p>data registred check your info page</p>
+    </div>
+  ';
+  }
+?>      
+<h1 style="font-size:23px">Register some infos</h1> 
+ <form action="" method="post">
+  <table>
+      <tr>
+        <td><label style="font-size:23px" for="inputtxt">Name</label></td>
+        <td><input style="width:200%" type="text" name="inputtxt" id="inputtxt"  required/><br></td>
+      </tr>
+      <tr>
+        <td><label style="font-size:23px" for="inputarea">Text Area</label></td>
+        <td><textarea style="width:200%" type="text" name="inputarea" id="inputarea"  required></textarea></td>
+      </tr>
+      <tr>
+        <td><label style="font-size:23px" for="option">options</label></td>
+        <td>
+          <select style="width:200%" name="option" id="option" required>
+            <option value="1">A</option>
+            <option value="2">B</option>
+            <option value="3">C</option>
+          </select>
+        </td>
+      </tr>
+      
+      
+    </table><br>
+    <input type="submit" class="button button-primary button-hero load-customize hide-if-no-customize" name="submit"/>
+</form>
+<?php
+
+}
+
+
+function MonPlugin_NouvellePage(){
+    
+    
+    echo '<h1 style="font-size:23px ">All Data</h1>';
+    
+    
+    global $wpdb;
+    $table_name = $wpdb->prefix . "plugin_table";
+    $results = $wpdb->get_results( "SELECT * FROM $table_name"); 
+    ?>
+   
+    <?php
+if(!empty($results))  
+{  
+    
+    
+    
+    
+    ?>  
+    
+    <table style="width:70%" class="wp-list-table widefat fixed striped pages text-center" >
+        <thead  >
+            <tr >
+                <th  >ID</th>
+                <th>Input Text</th>
+                <th>Input Area</th>
+                <th>Input option</th>
+            </tr>
+        </thead>
+        <tbody  id="the-list">
+    <?php
+          
+    foreach($results as $row){   
+        
+        ?>
+        <tr>
+            <td><?php  echo $row->id ; ?></td>
+            <td><?php echo $row->inputtxt ;?></td>
+            <td><?php echo $row->inputarea ;?></td>
+            <td><?php echo $row->option ;?></td>
+        </tr>
+     
+        <?php
+    }
+    ?>
+    <tbody>
+  </table>
+<?php
+}else{
+    echo 'there is no data for now';
+}
+?>
+    
+<?php
+}
+//runs when plugin is activated
+register_activation_hook( __FILE__, 'create_table' );
+
+function create_table(){
+    global $wpdb;
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+    $table_name = $wpdb->prefix . "plugin_table";  
+
+    $sql = "CREATE TABLE $table_name (
+      id int(10) unsigned NOT NULL AUTO_INCREMENT,
+      inputtxt varchar(255) NOT NULL,
+      inputarea varchar(255) NOT NULL,
+      option varchar(5) NOT NULL,
+      
+      PRIMARY KEY  (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+    dbDelta( $sql );
+}
+
+// Runs when plugin is desactivated
+register_deactivation_hook( __FILE__, 'remove_table' );
+
+function remove_table() {
+    global $wpdb;
+     $table_name = $wpdb->prefix . 'plugin_table';
+     $sql = "DROP TABLE IF EXISTS $table_name";
+     $wpdb->query($sql);
+     delete_option("my_plugin_db_version");
+} 
+
+?>
